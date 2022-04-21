8eba2305fe26cd429a1bb070675fb9ea353c3810
Leonardo Otero <leonardo.otero390@gmail.com>
2022-04-21 13:09:37 -0400

    feat: linter,prettier and husky

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -5,7 +5,7 @@ import * as userRepository from '../repositories/userRepository.js';
 
 export async function create({ email, password }: User) {
   const alreadyExists = await userRepository.findByEmail(email);
-  if (alreadyExists) throw new Conflict();
+  if (alreadyExists) throw new Conflict('Email already in use');
   const hashedPassword = bcrypt.hashSync(password, 10);
   await userRepository.create({
     email,
