62ca153992bcb7e4b075fafaccf38432d7f986cc
Leonardo Otero <leonardo.otero390@gmail.com>
2022-04-23 17:29:23 -0400

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import 'express-async-errors';
 import express from 'express';
+import cors from 'cors';
 import serverError from './middlewares/serverError.js';
 import routes from './routes/routes.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(serverError);
