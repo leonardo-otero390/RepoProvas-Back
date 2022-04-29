d4cbfb91e8e7c16eaada5e30129e81d87e31b06b
Leonardo Otero <leonardo.otero390@gmail.com>
2022-04-29 12:07:18 -0400

    feat: teachersRouter tests

--- src/routes/teacherRouter.ts
+++ src/routes/teacherRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import * as teacherController from '../controllers/teacherController.js';
+import * as testController from '../controllers/testController.js';
 
 const teacherRouter = Router();
 
 teacherRouter.get('/', teacherController.findMany);
+
+teacherRouter.get('/:teacherId/tests', testController.findManyByTeacherId);
 
 export default teacherRouter;
