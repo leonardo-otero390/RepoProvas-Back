f57191420d589985cf52ccf67edd49fce952407f
Leonardo Otero <leonardo.otero390@gmail.com>
2022-04-30 17:00:58 -0400

    feat: tests

--- tests/factories/testFactory.ts
+++ tests/factories/testFactory.ts
@@ -3,19 +3,32 @@ import { client } from '../../src/database.js';
 import * as categoryFactory from './categoryFactory.js';
 import * as teacherDisciplineFactory from './teachersDisciplineFactory.js';
 
-export async function create() {
+export async function generateData() {
   const year = faker.datatype.number({ min: 2000, max: 2022 });
   const semester = faker.datatype.number({ min: 1, max: 2 });
   const category = await categoryFactory.create();
-  const teacherDiscipline = await teacherDisciplineFactory.create();
-  const data = {
+  const {
+    teacherId,
+    disciplineId,
+    id: teacherDisciplineId,
+  } = await teacherDisciplineFactory.create();
+
+  return {
     name: `${year}.${semester}`,
     pdfUrl: faker.internet.url(),
     categoryId: category.id,
-    teacherDisciplineId: teacherDiscipline.id,
+    teacherDisciplineId,
+    teacherId,
+    disciplineId,
   };
+}
+
+export async function create() {
+  const data = await generateData();
+
+  const { teacherId, disciplineId, ...cleanData } = data;
 
   return client.test.create({
-    data,
+    data: cleanData,
   });
 }
--- tests/integration/testRouter.test.ts
+++ tests/integration/testRouter.test.ts
@@ -5,8 +5,13 @@ import * as testFactory from '../factories/testFactory';
 import * as utilsDatabase from '../utils/database';
 
 const agent = supertest(app);
+let token: string;
 beforeAll(async () => {
   await utilsDatabase.clearDatabase();
+});
+
+beforeEach(async () => {
+  token = await createValidToken(agent);
 });
 
 afterAll(async () => {
@@ -20,7 +25,6 @@ describe('PATCH /tests/:id/views', () => {
   });
 
   it('should return 400', async () => {
-    const token = await createValidToken(agent);
     const res = await agent
       .patch('/tests/id/views')
       .set({ authorization: `Bearer ${token}` });
@@ -28,12 +32,34 @@ describe('PATCH /tests/:id/views', () => {
   });
 
   it('should return 200', async () => {
-    const token = await createValidToken(agent);
     const test = await testFactory.create();
     const res = await agent
       .patch(`/tests/${test.id}/views`)
       .set({ authorization: `Bearer ${token}` });
     expect(res.status).toBe(200);
     expect(res.body.views).toEqual(test.views + 1);
+  });
+});
+
+describe('post /tests', () => {
+  it('should return 401', async () => {
+    const res = await agent.post('/tests');
+    expect(res.status).toBe(401);
+  });
+
+  it('should return 400', async () => {
+    const res = await agent
+      .post('/tests')
+      .set({ authorization: `Bearer ${token}` });
+    expect(res.status).toBe(400);
+  });
+
+  it('should return 201 succes', async () => {
+    const { teacherDisciplineId, ...data } = await testFactory.generateData();
+    const res = await agent
+      .post('/tests')
+      .send(data)
+      .set({ authorization: `Bearer ${token}` });
+    expect(res.status).toBe(201);
   });
 });
